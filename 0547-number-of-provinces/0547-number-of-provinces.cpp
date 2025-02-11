153c20a94107f86d7d50b6318c27b8e71ea4f169
YASHRAJ CHANDEL <118988314+Yashraj-71@users.noreply.github.com>
2025-02-11 10:00:24 +0530

    Time: 12 ms (8.27%), Space: 21.7 MB (8.88%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
+    // BFS to explore all nodes in the current connected component
+    void bfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
+        queue<int> q;
+        q.push(node);
+        vis[node] = 1;
 
-    void dfs(int node, vector<vector<int>>&adj,vector<int>&vis ){
-        vis[node]=1;
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
 
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                dfs(it, adj,vis);
-            }
-        }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        vector<vector<int>>adj(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+            for (auto neighbor : adj[current]) {
+                if (!vis[neighbor]) {
+                    vis[neighbor] = 1;
+                    q.push(neighbor);
                 }
             }
         }
-            int count=0;
-            vector<int>vis(n,0);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i, adj, vis);
-                count++;
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<vector<int>> adj(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i); 
+                }
+            }
+        }
+
+        int count = 0;
+        vector<int> vis(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                bfs(i, adj, vis);  
+                count++;  
             }
         }
 
