65aaa91452409674c5061d33f41841218fdc8db0
YASHRAJ CHANDEL <118988314+Yashraj-71@users.noreply.github.com>
2025-02-14 10:37:31 +0530

    Time: 3 ms (67.93%), Space: 8.2 MB (14.51%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n <= 1) return n;
-        int a = 0, b = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = b;
-            b = a + b;
-            a = temp;
+        vector<int>v1(n+1,-1);   // memoization
+        return fib1(n,v1);
+    }
+    int fib1(int n, vector<int>&v){
+        if(n <= 1){
+            v[n] = n;
+            return v[n];
         }
-        return b;
+        if(v[n] != -1){
+            return v[n];
+        }
+        v[n] = fib1(n-1,v) + fib1(n-2,v);
+        return v[n];
     }
 };
